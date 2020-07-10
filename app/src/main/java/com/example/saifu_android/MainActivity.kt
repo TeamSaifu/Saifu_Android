e9df3a3f3f557f0ed2a1f21916da74285bf1e96f
Nakafuji Mizuto <2180417@ecccomp.com>
2020-07-10 11:33:30 +0900

    ログ画面にのみメニューアイテムを表示

--- app/src/main/java/com/example/saifu_android/MainActivity.kt
+++ app/src/main/java/com/example/saifu_android/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.saifu_android
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.saifu_android.ui.log.logFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,8 +26,14 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_want, R.id.navigation_log,R.id.navigation_setting))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_want,
+                R.id.navigation_log,
+                R.id.navigation_setting
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
--- app/src/main/java/com/example/saifu_android/ui/log/logFragment.kt
+++ app/src/main/java/com/example/saifu_android/ui/log/logFragment.kt
@@ -1,13 +1,12 @@
 package com.example.saifu_android.ui.log
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.saifu_android.MainActivity
 import com.example.saifu_android.R
 
 class logFragment : Fragment() {
@@ -20,5 +19,15 @@ class logFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_log, container, false)
         return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        activity?.setTitle((R.menu.search_view))
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.search_view,menu)
     }
 }
