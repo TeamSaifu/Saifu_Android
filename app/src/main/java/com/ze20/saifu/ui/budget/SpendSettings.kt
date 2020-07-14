6241b71a4c83db0d0b1f347b481aa6b251258310
Nakafuji Mizuto <2180417@ecccomp.com>
2020-07-14 11:44:27 +0900

    固定支出画面のデザインを作成しました。

---
+++ app/src/main/java/com/ze20/saifu/ui/budget/SpendSettings.kt
@@ -0,0 +1,31 @@
+package com.ze20.saifu.ui.budget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import com.ze20.saifu.R
+
+class SpendSettings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_spend_settings)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setTitle("固定支出")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+
+        val inflater = menuInflater
+
+        inflater.inflate(R.menu.spend_menu, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return super.onSupportNavigateUp()
+    }
+}
