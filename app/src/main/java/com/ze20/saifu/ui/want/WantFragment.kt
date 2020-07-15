32f6e2410faf172a62d5ca28ca520fb3409c25a4
Takuya Shintani <2180264@ecccomp.com>
2020-07-15 11:02:18 +0900

    コードのフォーマット

--- app/src/main/java/com/ze20/saifu/ui/want/WantFragment.kt
+++ app/src/main/java/com/ze20/saifu/ui/want/WantFragment.kt
@@ -1,16 +1,20 @@
 package com.ze20.saifu.ui.want
 
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ze20.saifu.R
 
 class WantFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_want, container, false)
         return root
@@ -23,6 +27,6 @@ class WantFragment : Fragment() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.menu_wishlist,menu)
+        inflater.inflate(R.menu.menu_wishlist, menu)
     }
 }
