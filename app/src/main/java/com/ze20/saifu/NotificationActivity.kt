36a7277d4c3660cf977fd8d179b2e879b6d87277
Toyoda Keita <2180144@ecccomp.com>
2020-07-28 10:47:54 +0900

    //の後にスペースを追加

--- app/src/main/java/com/ze20/saifu/NotificationActivity.kt
+++ app/src/main/java/com/ze20/saifu/NotificationActivity.kt
@@ -30,10 +30,10 @@ class NotificationActivity : AppCompatActivity() {
             notificationManager.createNotificationChannel(mChannel)
         }
 
-        //インテントを作成(通知をタップした時に表示する画面???)
+        // インテントを作成(通知をタップした時に表示する画面???)
         val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
 
-        //通知のアイコン、タイトル、本文、インテント(表示する画面)、trueでタップした通知は消える
+        // 通知のアイコン、タイトル、本文、インテント(表示する画面)、trueでタップした通知は消える
         var builder = NotificationCompat.Builder(this, "2")
             .setSmallIcon(R.drawable.ic_baseline_notifications_48)
             .setContentTitle("通知テスト")
@@ -42,7 +42,7 @@ class NotificationActivity : AppCompatActivity() {
             .setContentIntent(pendingIntent)
             .setAutoCancel(true)
 
-        //ボタンを押すと通知がステータスバーに表示される
+        // ボタンを押すと通知がステータスバーに表示される
         tsuchiButton.setOnClickListener {
             with(NotificationManagerCompat.from(this)) {
                 notify(1, builder.build())
