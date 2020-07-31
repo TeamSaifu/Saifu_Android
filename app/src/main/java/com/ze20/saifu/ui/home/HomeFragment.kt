ff21162619c3ea0e40920e7b065fe7499bd3d4f7
AKUTSU TAKUYA <2180407@ecccomp.com>
2020-07-31 11:27:22 +0900

    ベルマーク画面遷移の追加

--- app/src/main/java/com/ze20/saifu/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ze20/saifu/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.ze20.saifu.ui.home
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager.widget.PagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
+import com.ze20.saifu.NotificationActivity
 import com.ze20.saifu.R
 
 class HomeFragment : Fragment() {
@@ -42,5 +44,15 @@ class HomeFragment : Fragment() {
         // tablayoutを表示
         tablayout.setupWithViewPager(pager, true)
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        NC_transition.setOnClickListener {
+            startActivity(Intent(activity, NotificationActivity::class.java))
+
+        }
     }
 }
