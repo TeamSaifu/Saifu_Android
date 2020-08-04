8122e82ef1909a19fb36da7ce33617c0ae6e35f3
Takuya Shintani <2180264@ecccomp.com>
2020-08-04 13:04:44 +0900

    GraphActivityを作成

---
+++ app/src/main/java/com/ze20/saifu/GraphActivity.kt
@@ -0,0 +1,11 @@
+package com.ze20.saifu
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GraphActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_graph)
+    }
+}
