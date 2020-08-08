ba946a27d19116543b790ca66127e399c701724f
Takuya Shintani <2180264@ecccomp.com>
2020-08-08 22:31:51 +0900

    ウィジェットのクリックした時の動作を設定

--- app/src/main/java/com/ze20/saifu/SaifuAddWidget.kt
+++ app/src/main/java/com/ze20/saifu/SaifuAddWidget.kt
@@ -6,46 +6,54 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.widget.RemoteViews
-import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
+import com.ze20.saifu.ui.input.DataInputActivity
+
+// https://qiita.com/shiita0903/items/6292e462dea749ba03af
+// https://qiita.com/Pinehead/items/d83c84e9308969b59654
 
 /**
- * Implementation of App Widget functionality.
+ * アプリウィジェット機能の実装。
  */
 class SaifuAddWidget : AppWidgetProvider() {
+
+    companion object {
+        const val CLICK_ACTION = "CLICK_WIDGET"
+    }
+
+    internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+        // Construct the RemoteViews object
+        val views = RemoteViews(context.packageName, R.layout.saifu_add_widget)
+
+        val intent = Intent(context, SaifuAddWidget::class.java).apply {
+            action = CLICK_ACTION
+        }
+        val pIntent = PendingIntent.getBroadcast(context, appWidgetId, intent, 0)
+        views.setOnClickPendingIntent(R.id.penImage, pIntent)
+        // ウィジェットを更新するようにウィジェットマネージャーに指示する
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
+
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
-        // There may be multiple widgets active, so update all of them
+        // 複数のウィジェットがアクティブになっている可能性があるため、それらをすべて更新してください
         for (appWidgetId in appWidgetIds) {
-            val views = RemoteViews(context.packageName, R.layout.saifu_add_widget)
-            views.setOnClickPendingIntent(R.id.widgetFrame, clickAction(context));
             updateAppWidget(context, appWidgetManager, appWidgetId)
         }
     }
 
     override fun onEnabled(context: Context) {
-        // Enter relevant functionality for when the first widget is created
+        // 最初のウィジェットが作成されたときの関連機能を入力します
     }
 
     override fun onDisabled(context: Context) {
-        // Enter relevant functionality for when the last widget is disabled
+        // 最後のウィジェットが無効になったときの関連機能を入力します
     }
 
-    override fun onReceive(context: Context?, intent: Intent?) {
-        super.onReceive(context, intent)
-        if (intent?.action == "click_action") {
-            Toast.makeText(context, "click_action", Toast.LENGTH_SHORT).show();
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent) // ACTION_APPWIDGET_UPDATEなどのIntentが処理される
+        if (intent.action.equals(CLICK_ACTION)) {
+            startActivity(context, Intent(context, DataInputActivity::class.java), null)
         }
     }
-
-    private fun clickAction(context: Context): PendingIntent? {
-        val intent = Intent("click_action")
-        return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-    }
 }
 
-internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.saifu_add_widget)
-
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
-}
