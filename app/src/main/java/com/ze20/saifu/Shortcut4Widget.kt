4de5f8aefb4b67513c162db89910e8d5799ed955
Takuya Shintani <2180264@ecccomp.com>
2020-08-10 22:50:42 +0900

    ショートカットウィジェットの実装

--- app/src/main/java/com/ze20/saifu/Shortcut4Widget.kt
+++ app/src/main/java/com/ze20/saifu/Shortcut4Widget.kt
@@ -1,15 +1,30 @@
 package com.ze20.saifu
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.view.View
 import android.widget.RemoteViews
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
+import androidx.preference.PreferenceManager
+import com.ze20.saifu.ui.input.DataInputActivity
 
 /**
  * Implementation of App Widget functionality.
  */
 class Shortcut4Widget : AppWidgetProvider() {
+
+    companion object {
+        const val BUTTON1 = "SHORTCUT_1"
+        const val BUTTON2 = "SHORTCUT_2"
+        const val BUTTON3 = "SHORTCUT_3"
+        const val BUTTON4 = "SHORTCUT_4"
+    }
+
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
         // 複数のウィジェットがアクティブになっている可能性があるため、それらをすべて更新してください
         for (appWidgetId in appWidgetIds) {
@@ -24,61 +39,105 @@ class Shortcut4Widget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // 最後のウィジェットが無効になったときの関連機能を入力します
     }
+
+    override fun onReceive(context: Context, intent: Intent) {
+        super.onReceive(context, intent)
+        when (intent.action) {
+            BUTTON1 -> shortcut(context, intent)
+            BUTTON2 -> shortcut(context, intent)
+            BUTTON3 -> shortcut(context, intent)
+            BUTTON4 -> shortcut(context, intent)
+        }
+    }
+
+    private fun shortcut(context: Context, intent: Intent) {
+        val sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+        val name = intent.getStringExtra("name")
+        val price = intent.getIntExtra("price", 0)
+        val category = intent.getIntExtra("category", 0)
+        Log.e("widget", "$name : $price : $category")
+        if (sharedPref.getBoolean("shortCutQuickAdd", false)) {
+            if (ConvenientFunction().quickInsert(context, price, name!!, category)) {
+                Toast.makeText(context, context.getString(R.string.recordFinish), Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(context, context.getString(R.string.recordError), Toast.LENGTH_LONG).show()
+            }
+        } else {
+            val dataIntent = Intent(context, DataInputActivity::class.java)
+            dataIntent.putExtra("mode", "Shortcut")
+            dataIntent.putExtra("price", price)
+            dataIntent.putExtra("name", name)
+            dataIntent.putExtra("category", category)
+            startActivity(context, dataIntent, null)
+        }
+    }
+
+    private fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+        // RemoteViewsオブジェクトを構築する
+        val views = RemoteViews(context.packageName, R.layout.shortcut4_widget)
+        val sqLiteDB = SQLiteDB(context, "SaifuDB", null, 1)
+        val database = sqLiteDB.readableDatabase
+        val sql = "select * from shortcut order by 1 asc limit 4;"
+        val cursor = database.rawQuery(sql, null)
+        val price: ArrayList<Int> = arrayListOf()
+        val name: ArrayList<String> = arrayListOf()
+        val category: ArrayList<Int> = arrayListOf()
+        val shortcut: ArrayList<Int> =
+            arrayListOf(R.id.shurtcut_button1, R.id.shurtcut_button2, R.id.shurtcut_button3, R.id.shurtcut_button4)
+        var i = 0
+        if (cursor.count > 0) {
+            cursor.moveToFirst()
+            if (cursor.count <= 2) {
+                views.setViewVisibility(R.id.layout2, View.GONE)
+            }
+            while (!cursor.isAfterLast) {
+                views.setViewVisibility(R.id.textView, View.GONE)
+                views.setViewVisibility(R.id.button4Layout, View.VISIBLE)
+                if (cursor.getString(1) == "") {
+                    views.setTextViewText(shortcut[i], context.getString(R.string.currencyString, "%,d".format(cursor.getInt(2))))
+                } else {
+                    views.setTextViewText(shortcut[i], context.getString(R.string.shortcutformat, cursor.getString(1), "%,d".format(cursor.getInt(2))))
+                }
+                price.add(cursor.getInt(2))
+                name.add(cursor.getString(1))
+                category.add(cursor.getInt(3))
+                views.setViewVisibility(shortcut[i], View.VISIBLE)
+                i++
+                cursor.moveToNext()
+            }
+        }
+        cursor.close()
+        try {
+            views.setOnClickPendingIntent(R.id.shurtcut_button1, PendingIntent.getBroadcast(context, appWidgetId, Intent(context, Shortcut4Widget::class.java).apply {
+                action = BUTTON1
+                putExtra("price", price[0])
+                putExtra("name", name[0])
+                putExtra("category", category[0])
+            }, 0))
+            views.setOnClickPendingIntent(R.id.shurtcut_button2, PendingIntent.getBroadcast(context, appWidgetId, Intent(context, Shortcut4Widget::class.java).apply {
+                action = BUTTON2
+                putExtra("price", price[1])
+                putExtra("name", name[1])
+                putExtra("category", category[1])
+            }, 0))
+            views.setOnClickPendingIntent(R.id.shurtcut_button3, PendingIntent.getBroadcast(context, appWidgetId, Intent(context, Shortcut4Widget::class.java).apply {
+                action = BUTTON3
+                putExtra("price", price[2])
+                putExtra("name", name[2])
+                putExtra("category", category[2])
+            }, 0))
+            views.setOnClickPendingIntent(R.id.shurtcut_button4, PendingIntent.getBroadcast(context, appWidgetId, Intent(context, Shortcut4Widget::class.java).apply {
+                action = BUTTON4
+                putExtra("price", price[3])
+                putExtra("name", name[3])
+                putExtra("category", category[3])
+            }, 0))
+        } catch (e: IndexOutOfBoundsException) {
+
+        }
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
 }
 
-internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
-    // RemoteViewsオブジェクトを構築する
-    val views = RemoteViews(context.packageName, R.layout.shortcut4_widget)
-    val SQLiteDB = SQLiteDB(context, "SaifuDB", null, 1)
-    val database = SQLiteDB.readableDatabase
-    val sql = "select * from shortcut order by 1 asc limit 4;"
-    val cursor = database.rawQuery(sql, null)
-    val shortcut: ArrayList<Int> =
-        arrayListOf(R.id.shurtcut_button1, R.id.shurtcut_button2, R.id.shurtcut_button3, R.id.shurtcut_button4)
-    var i = 0
-    if (cursor.count > 0) {
-        cursor.moveToFirst()
-        if (cursor.count <= 2) {
-            views.setViewVisibility(R.id.layout2, View.GONE)
-        }
-        while (!cursor.isAfterLast) {
-            views.setViewVisibility(R.id.textView, View.GONE)
-            views.setViewVisibility(R.id.button4Layout, View.VISIBLE)
-            if (cursor.getString(1) == "") {
-                views.setTextViewText(shortcut[i], context.getString(R.string.currencyString, "%,d".format(cursor.getInt(2))))
-                //views.setTextViewTextSize(shortcut[i], 0, 72.0f)
-            } else {
-                views.setTextViewText(shortcut[i], context.getString(R.string.shortcutformat, cursor.getString(1), "%,d".format(cursor.getInt(2))))
-            }
-            views.setViewVisibility(shortcut[i], View.VISIBLE)
-            /*shortcut[i].setOnClickListener {
-                val sharedPref = PreferenceManager.getDefaultSharedPreferences(activity)
-                if (sharedPref.getBoolean("shortCutQuickAdd", false)) {
-                    val tag = it.tag as Int
-                    it.isEnabled = false
-                    if (ConvenientFunction().quickInsert(context, priceal[tag], nameal[tag], categoryal[tag])) {
-                        Toast.makeText(activity, getString(R.string.recordFinish), Toast.LENGTH_LONG).show()
-                        it.isEnabled = true
-                    } else {
-                        Toast.makeText(activity, getString(R.string.recordError), Toast.LENGTH_LONG).show()
-                        it.isEnabled = true
-                    }
-                } else {
-                    val intent = Intent(activity, DataInputActivity::class.java)
-                    val tag = it.tag as Int
-                    intent.putExtra("mode", "Shortcut")
-                    intent.putExtra("name", nameal[tag])
-                    intent.putExtra("price", priceal[tag])
-                    intent.putExtra("category", categoryal[tag])
-                    startActivity(intent)
-                }
-            }*/
-            i++
-            cursor.moveToNext()
-        }
-    } else {
-    }
-    cursor.close()
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
-}
+
